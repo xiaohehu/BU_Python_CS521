e5e27c594ba0f4922112b6b10e9030846d7b86d5
Xiaohe Hu <huxiaohe113@gmail.com>
2017-03-27 00:49:56 -0400

    Modified lab4

--- week8/Hu_Xiaohe_Lab4.py
+++ week8/Hu_Xiaohe_Lab4.py
@@ -4,13 +4,13 @@ class MyException(Exception):
 		print('Found # Error!')
 	
 	def __str__(self):
-		return ('Found the symbol #!')
+		return ('Found the symbol # from the input string!')
 
 def get_name():
 	print("Please enter a name (if it contains a '#', an error message will appear: ")
 	name_input = input()
 	if name_input.find('#') > -1:
-		raise(MyException())
+		raise(MyException)
 
 # EXECUTION
 
